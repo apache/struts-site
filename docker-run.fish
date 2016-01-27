80f0d7b0b410f41ab8ba2d8275d0a252a5948c3a
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-01-27 09:20:40 +0100

    Modifies how Docker is launched

--- docker-run.fish
+++ docker-run.fish
@@ -1,2 +1,2 @@
-docker run -v $PWD:/srv/jekyll -t -p 4000:4000 theapachestruts/struts-site-jekyll bundle exec jekyll serve --watch
+docker run -v $PWD:/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll bundle exec jekyll serve --watch
 
