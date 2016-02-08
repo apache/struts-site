2bb4bd1ca461ee63d10da6f48ec0e792f7f554bd
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-02-08 08:19:42 +0100

    Adds proper host binding

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -v $(pwd):/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll bundle exec jekyll serve --watch
+docker run -v $(pwd):/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll bundle exec jekyll serve --watch --trace --host=0.0.0.0
 
