b33eee403faa382bcfb316617d6bafca9ae06ecb
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-03-04 11:52:09 +0100

    Simplifies run scripts

--- docker-run.fish
+++ docker-run.fish
@@ -1,2 +1,2 @@
 #!/usr/local/bin/fish
-docker run -v $PWD:/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll bundle exec jekyll serve --watch --trace --host=0.0.0.0 --force_polling
+docker run -v $PWD:/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll
--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -v $(pwd):/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll bundle exec jekyll serve --watch --trace --host=0.0.0.0 --force_polling
+docker run -v $(pwd):/srv/jekyll -it -p 4000:4000 theapachestruts/struts-site-jekyll
