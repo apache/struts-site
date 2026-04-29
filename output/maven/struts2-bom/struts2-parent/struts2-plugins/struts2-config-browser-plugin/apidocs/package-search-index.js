2412ce8840705f57ad998acd50713d483781f8dd
buildbot <users@infra.apache.org>
2026-04-29 06:04:49 +0000

    Automatic Site Publish by Buildbot

--- output/maven/struts2-bom/struts2-parent/struts2-plugins/struts2-config-browser-plugin/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.struts2.config_browser"}];updateSearchResults();
--- output/maven/struts2-bom/struts2-parent/struts2-plugins/struts2-convention-plugin/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.struts2.convention"},{"l":"org.apache.struts2.convention.annotation"},{"l":"org.apache.struts2.convention.config.entities"}];updateSearchResults();
