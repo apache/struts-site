90bd96b60233ead1d4cb000b310357b2a450bf71
buildbot <users@infra.apache.org>
2026-08-14 07:44:35 +0000

    Automatic Site Publish by Buildbot

--- output/maven/struts2-plugins/struts2-cdi-plugin/apidocs/type-search-index.js
+++ output/maven/struts2-plugins/struts2-cdi-plugin/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.struts2.cdi","l":"CdiObjectFactory"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.struts2.cdi","l":"CdiObjectFactory"},{"p":"org.apache.struts2.cdi","l":"CdiProxyService"}];updateSearchResults();
